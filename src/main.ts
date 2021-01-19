ba374fc13859332d3dd94741ea0e21312d3c88b9
Jakub Jelínek <jakubkoje@gmail.com>
2021-01-19 21:17:14 +0100

    added vuex to project

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { createApp } from 'vue';
 import { IonicVue } from '@ionic/vue';
 import App from './App.vue';
 import router from './router';
+import store from './store';
 
 import './plugins';
 
 const app = createApp(App)
   .use(IonicVue, { mode: 'ios' })
-  .use(router);
+  .use(router)
+  .use(store);
 
 router.isReady().then(() => {
   app.mount('#app');
