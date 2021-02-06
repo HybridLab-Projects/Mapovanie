6718d9d94877d23025f073b757450361b0d62fc4
Mark Mucska <mark.mucska.work@gmail.com>
2021-02-06 22:22:34 +0100

    Settings view

---
+++ src/plugins/app/settings/qna.vue
@@ -0,0 +1,28 @@
+<template>
+  <ion-page>
+    <i-header title="Otazky a odpovede" />
+    <ion-content class="ion-padding">
+      <h1>Otázky</h1>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import {
+  IonPage,
+  IonContent,
+} from '@ionic/vue';
+
+export default defineComponent({
+  name: 'Otazky',
+  components: {
+    IonContent,
+    IonPage,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/plugins/app/settings/settings.vue
@@ -0,0 +1,59 @@
+<template>
+  <ion-page>
+    <i-header title="Nastavenia" />
+    <ion-content class="ion-padding">
+      <ion-list>
+        <ion-item>
+          <ion-label>Jazyk</ion-label>
+          <ion-select
+            value="sk"
+            placeholder="Jazyk"
+          >
+            <ion-select-option value="sk">
+              Slovenčina
+            </ion-select-option>
+            <ion-select-option value="eng">
+              English
+            </ion-select-option>
+          </ion-select>
+        </ion-item>
+
+        <ion-item router-link="/tabs/settings/qna">
+          <ion-label>
+            Otázky a odpovede
+          </ion-label>
+        </ion-item>
+      </ion-list>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import {
+  IonPage,
+  IonContent,
+  IonList,
+  IonItem,
+  IonLabel,
+} from '@ionic/vue';
+
+export default defineComponent({
+  name: 'Settings',
+  //   data() {
+  //       return {
+  //       },
+  //   },
+  components: {
+    IonContent,
+    IonPage,
+    IonList,
+    IonItem,
+    IonLabel,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
