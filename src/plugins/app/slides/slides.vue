fe01e41c04fbf5068302a0f44996373032ea203f
Mark Mucska <mark.mucska.work@gmail.com>
2021-02-16 20:41:29 +0100

    slides update

--- src/plugins/app/slides/slides.vue
+++ src/plugins/app/slides/slides.vue
@@ -1,44 +1,40 @@
 <template>
   <ion-page>
-    <ion-content class="ion-padding ion-text-center">
+    <ion-content class="ion-text-center">
       <ion-slides pager="true">
         <ion-slide>
-          <div class="slide">
+          <div class="ion-padding">
             <ion-img :src="require('./img/welcome.svg')" />
-            <h2>Welcome</h2>
-            <p>
-              The ionic conference app is a practical
-              preview of the ionic framework in action,
-              and a demonstration of proper code use.
-            </p>
+            <div>
+              <h2>Welcome</h2>
+              <p>
+                The ionic conference app is a practical
+                preview of the ionic framework in action,
+                and a demonstration of proper code use.
+              </p>
+            </div>
           </div>
         </ion-slide>
 
         <ion-slide>
-          <div class="slide">
+          <div class="ion-padding">
             <ion-img :src="require('./img/question.svg')" />
-            <h2>What is Ionic?</h2>
-            <p>
-              Ionic Framework is an open source
-              SDK that enables developers to
-              build high quality mobile apps with web technologies
-              like HTML, CSS, and JavaScript.
-            </p>
+            <div>
+              <h2>What is Ionic?</h2>
+              <p>
+                Ionic Framework is an open source
+                SDK that enables developers to
+                build high quality mobile apps with web technologies
+                like HTML, CSS, and JavaScript.
+              </p>
+            </div>
           </div>
         </ion-slide>
 
         <ion-slide>
-          <div class="slide">
+          <div class="ion-padding">
             <ion-img :src="require('./img/start.svg')" />
             <h2>Ready to Play?</h2>
-            <ion-item lines="none">
-              <p>Dont show again</p>
-              <ion-checkbox
-                slot="start"
-                color="primary"
-                checked
-              />
-            </ion-item>
             <ion-button
               fill="clear"
               router-link="/"
@@ -66,8 +62,6 @@ import {
   IonSlide,
   IonButton,
   IonImg,
-  IonItem,
-  IonCheckbox,
 } from '@ionic/vue';
 
 import { arrowForwardOutline } from 'ionicons/icons';
@@ -81,8 +75,6 @@ export default defineComponent({
     IonSlide,
     IonButton,
     IonImg,
-    IonItem,
-    IonCheckbox,
   },
   data() {
     return {
@@ -91,3 +83,9 @@ export default defineComponent({
   },
 });
 </script>
+
+<style scoped>
+  ion-slides {
+    height: 100%;
+  }
+</style>
