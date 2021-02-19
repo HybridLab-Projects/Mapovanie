9d8b74b2ffddb1d9c0ed16407068f73d982e6a34
Mark Mucska <mark.mucska.work@gmail.com>
2021-02-19 12:57:08 +0100

    leaderboard

--- src/plugins/app/latest/leaderboard.vue
+++ src/plugins/app/latest/leaderboard.vue
@@ -5,14 +5,20 @@
   >
     <ion-refresher-content pulling-icon="lines" />
   </ion-refresher>
-  <ion-item
-    v-for="num in itemCount"
-    :key="num"
-  >
-    <ion-label>
-      Item {{ num }}
-    </ion-label>
-  </ion-item>
+  <ion-list>
+    <ion-item
+      v-for="(user, i) in users"
+      :key="i"
+    >
+      <ion-avatar slot="start">
+        <img :src="user.avatar">
+      </ion-avatar>
+      <ion-label>
+        <h3>{{ user.username }}</h3>
+        <p>{{ user.description }}</p>
+      </ion-label>
+    </ion-item>
+  </ion-list>
 </template>
 
 <script lang="ts">
@@ -22,7 +28,9 @@ import {
   IonRefresher,
   IonRefresherContent,
   IonItem,
+  IonList,
   IonLabel,
+  IonAvatar,
 } from '@ionic/vue';
 
 export default defineComponent({
@@ -30,23 +38,39 @@ export default defineComponent({
   components: {
     IonRefresher,
     IonRefresherContent,
+    IonList,
     IonItem,
     IonLabel,
+    IonAvatar,
   },
   data() {
     return {
+      users: [
+        {
+          avatar: 'https://avatars.dicebear.com/4.5/api/male/jakub.svg',
+          id: 1,
+          username: 'Jakub',
+          description: 'Lorem',
+        },
+        {
+          avatar: 'https://avatars.dicebear.com/4.5/api/male/jaro.svg',
+          id: 1,
+          username: 'Jaro',
+          description: 'Lorem',
+        },
+      ],
       itemCount: 2,
     };
   },
   methods: {
-    doRefresh(e: CustomEvent) {
-      setTimeout(() => {
-        console.log('Async operation has ended');
-        this.itemCount += 2;
-        // @ts-expect-error
-        e.target.complete();
-      }, 1000);
-    },
+    // doRefresh(e: CustomEvent) {
+    //   setTimeout(() => {
+    //     console.log('Async operation has ended');
+    //     this.itemCount += 2;
+    //     // @ts-expect-error
+    //     e.target.complete();
+    //   }, 1000);
+    // },
   },
 });
 </script>
