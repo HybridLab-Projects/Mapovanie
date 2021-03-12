f0ae0f6c5403a7d65a2465d4f742f7986ae12ab9
jakubkoje <jakubkoje@gmail.com>
2021-03-12 20:46:08 +0100

    fixed router name

--- src/plugins/app/_config/router.ts
+++ src/plugins/app/_config/router.ts
@@ -37,7 +37,7 @@ const routes: Array<RouteRecordRaw> = [
         component: () => import('@/plugins/app/latest/latest.vue'),
       },
       {
-        name: 'ChooseCategory',
+        name: 'Categories',
         path: 'categories',
         component: () => import('@/plugins/app/categories/categories.vue'),
       },
