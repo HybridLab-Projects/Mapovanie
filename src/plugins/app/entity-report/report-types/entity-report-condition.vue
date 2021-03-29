1f0605bb5f918fbbaf341ab6b0671dc3ba48c03a
Mark Mucska <mark.mucska.work@gmail.com>
2021-03-29 19:33:33 +0200

    added report screens

---
+++ src/plugins/app/entity-report/report-types/entity-report-condition.vue
@@ -0,0 +1,50 @@
+<template>
+  <ion-header>
+    <ion-toolbar>
+      <ion-buttons slot="start">
+        <ion-back-button @click="goBack()" />
+      </ion-buttons>
+      <ion-title>Nahlásenie stavu</ion-title>
+    </ion-toolbar>
+  </ion-header>
+  <ion-content>
+    <h1>Stav</h1>
+    <ion-button @click="test()">
+      test
+    </ion-button>
+  </ion-content>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue'
+import {
+  IonHeader, IonToolbar, IonButtons, IonContent, IonTitle, IonBackButton, IonButton,
+} from '@ionic/vue'
+
+export default defineComponent({
+  name: 'EntityReportCondition',
+  components: {
+    IonHeader, IonToolbar, IonButtons, IonContent, IonTitle, IonBackButton, IonButton,
+  },
+  props: {
+    entity: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    goBack() {
+      const nav = document.querySelector('ion-nav')
+      if (!nav) return
+      nav.pop()
+    },
+    test() {
+      console.log(this.entity)
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/plugins/app/entity-report/report-types/entity-report-content.vue
@@ -0,0 +1,53 @@
+<template>
+  <ion-header>
+    <ion-toolbar>
+      <ion-buttons slot="start">
+        <ion-back-button @click="goBack()" />
+      </ion-buttons>
+      <ion-title>Nahlásenie obsahu</ion-title>
+    </ion-toolbar>
+  </ion-header>
+  <ion-content>
+    <h1>Obsah</h1>
+    <ion-textarea />
+  </ion-content>
+  <ion-footer>
+    <ion-button expand="block" class="ion-margin">
+      Odoslať
+    </ion-button>
+  </ion-footer>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue'
+import {
+  IonHeader, IonToolbar, IonButtons, IonContent, IonTitle, IonBackButton, IonFooter, IonTextarea,
+} from '@ionic/vue'
+
+export default defineComponent({
+  name: 'EntityReportContent',
+  components: {
+    IonHeader, IonToolbar, IonButtons, IonContent, IonTitle, IonBackButton, IonFooter, IonTextarea,
+  },
+  props: {
+    entity: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    goBack() {
+      const nav = document.querySelector('ion-nav')
+      if (!nav) return
+      nav.pop()
+    },
+    test() {
+      console.log(this.entity)
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
