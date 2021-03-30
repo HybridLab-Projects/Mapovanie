a406e1b6e03f8a9c7d12117f8eedd0efc93e9872
Mark Mucska <mark.mucska.work@gmail.com>
2021-03-30 10:49:03 +0200

    reports

--- src/plugins/app/entity-report/report-types/entity-report-condition.vue
+++ src/plugins/app/entity-report/report-types/entity-report-condition.vue
@@ -7,25 +7,48 @@
       <ion-title>Nahlásenie stavu</ion-title>
     </ion-toolbar>
   </ion-header>
-  <ion-content>
-    <h1>Stav</h1>
-    <ion-button @click="test()">
-      test
-    </ion-button>
+  <ion-content class="ion-padding-horizontal">
+    <h1>Zmena stavu</h1>
+    <ion-item class="ion-no-padding">
+      <ion-label position="floating">
+        Opíšte problém
+      </ion-label>
+      <ion-textarea
+        v-model="reportMessage"
+        auto-grow
+        maxlength="500"
+      />
+    </ion-item>
   </ion-content>
+  <ion-footer>
+    <ion-button expand="block" class="ion-margin" @click="reportEntity">
+      Odoslať
+    </ion-button>
+  </ion-footer>
 </template>
 
 <script lang="ts">
 
 import { defineComponent } from 'vue'
 import {
-  IonHeader, IonToolbar, IonButtons, IonContent, IonTitle, IonBackButton, IonButton,
+  IonHeader, IonToolbar, IonFooter, IonButtons, IonContent,
+  IonTitle, IonBackButton, IonButton, loadingController, alertController, IonTextarea, IonItem,
 } from '@ionic/vue'
+import EntityReportSuccessView from '@/plugins/app/entity-report/entity-report-success.vue'
 
 export default defineComponent({
   name: 'EntityReportCondition',
   components: {
-    IonHeader, IonToolbar, IonButtons, IonContent, IonTitle, IonBackButton, IonButton,
+    IonHeader,
+    IonToolbar,
+    IonFooter,
+    IonButtons,
+    IonContent,
+    IonTitle,
+    IonBackButton,
+    IonButton,
+    IonTextarea,
+    IonItem,
   },
   props: {
     entity: {
@@ -33,14 +56,45 @@ export default defineComponent({
       required: true,
     },
   },
+  data() {
+    return {
+      reportMessage: '',
+    }
+  },
   methods: {
     goBack() {
       const nav = document.querySelector('ion-nav')
       if (!nav) return
       nav.pop()
     },
-    test() {
-      console.log(this.entity)
+    async reportEntity() {
+      const loading = await loadingController.create({
+        message: 'Odosielam...',
+      })
+      try {
+        await loading.present()
+        await this.$store.dispatch('reportEntity', {
+          entityId: this.entity.id,
+          type: 'broken',
+          content: this.reportMessage,
+        })
+        await loading.dismiss()
+
+        const ionNav = document.querySelector('ion-nav')
+        if (!ionNav) return
+        ionNav.push(EntityReportSuccessView)
+      } catch (err) {
+        await loading.dismiss()
+
+        const alert = await alertController
+          .create({
+            cssClass: 'my-custom-class',
+            header: 'Error',
+            message: err.message || err,
+            buttons: ['OK'],
+          })
+        await alert.present()
+      }
     },
   },
 })
--- src/plugins/app/entity-report/report-types/entity-report-content.vue
+++ src/plugins/app/entity-report/report-types/entity-report-content.vue
@@ -7,12 +7,21 @@
       <ion-title>Nahlásenie obsahu</ion-title>
     </ion-toolbar>
   </ion-header>
-  <ion-content>
-    <h1>Obsah</h1>
-    <ion-textarea />
+  <ion-content class="ion-padding-horizontal">
+    <h1>Nevhodný obsah</h1>
+    <ion-item class="ion-no-padding">
+      <ion-label position="floating">
+        Opíšte problém
+      </ion-label>
+      <ion-textarea
+        v-model="reportMessage"
+        auto-grow
+        maxlength="500"
+      />
+    </ion-item>
   </ion-content>
   <ion-footer>
-    <ion-button expand="block" class="ion-margin">
+    <ion-button expand="block" class="ion-margin" @click="reportEntity">
       Odoslať
     </ion-button>
   </ion-footer>
@@ -22,13 +31,25 @@
 
 import { defineComponent } from 'vue'
 import {
-  IonHeader, IonToolbar, IonButtons, IonContent, IonTitle, IonBackButton, IonFooter, IonTextarea,
+  IonHeader, IonToolbar, IonButtons, IonButton, IonContent, IonTitle, IonBackButton,
+  IonFooter, IonTextarea, loadingController, alertController, IonItem, IonLabel,
 } from '@ionic/vue'
+import EntityReportSuccessView from '@/plugins/app/entity-report/entity-report-success.vue'
 
 export default defineComponent({
   name: 'EntityReportContent',
   components: {
-    IonHeader, IonToolbar, IonButtons, IonContent, IonTitle, IonBackButton, IonFooter, IonTextarea,
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonContent,
+    IonTitle,
+    IonBackButton,
+    IonFooter,
+    IonTextarea,
+    IonButton,
+    IonItem,
+    IonLabel,
   },
   props: {
     entity: {
@@ -36,14 +57,45 @@ export default defineComponent({
       required: true,
     },
   },
+  data() {
+    return {
+      reportMessage: '',
+    }
+  },
   methods: {
     goBack() {
       const nav = document.querySelector('ion-nav')
       if (!nav) return
       nav.pop()
     },
-    test() {
-      console.log(this.entity)
+    async reportEntity() {
+      const loading = await loadingController.create({
+        message: 'Odosielam...',
+      })
+      try {
+        await loading.present()
+        await this.$store.dispatch('reportEntity', {
+          entityId: this.entity.id,
+          type: 'nsfw',
+          content: this.reportMessage,
+        })
+        await loading.dismiss()
+
+        const ionNav = document.querySelector('ion-nav')
+        if (!ionNav) return
+        ionNav.push(EntityReportSuccessView)
+      } catch (err) {
+        await loading.dismiss()
+
+        const alert = await alertController
+          .create({
+            cssClass: 'my-custom-class',
+            header: 'Error',
+            message: err.message || err,
+            buttons: ['OK'],
+          })
+        await alert.present()
+      }
     },
   },
 })
