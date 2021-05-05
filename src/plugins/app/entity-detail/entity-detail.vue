81d4815522390e25d32e366846be7182d89c44ce
Jakub Jelínek <jakubkoje@gmail.com>
2021-05-05 17:35:46 +0200

    cleaned code

--- src/plugins/app/entity-detail/entity-detail.vue
+++ src/plugins/app/entity-detail/entity-detail.vue
@@ -5,8 +5,8 @@
         <ion-buttons slot="start">
           <ion-back-button />
         </ion-buttons>
-        <ion-title v-if="entity?.type === 'bench'">
-          Lavička
+        <ion-title>
+          Lavička *
         </ion-title>
         <ion-buttons slot="end">
           <ion-button @click="openReportModal()">
@@ -19,64 +19,61 @@
       </ion-toolbar>
     </ion-header>
 
-    <ion-content>
+    <ion-content class="ion-padding">
       <ion-img
-        class="image ion-margin"
+        class="image-border-radius ion-margin-bottom"
         :src="entity?.images[0]?.url"
       />
-      <div class="ion-padding">
-        <ion-text
-          color="medium"
-          class="ion-text-center"
-        >
-          <h5 class="font-weight-bold ion-align-self-center ion-no-margin">
-            {{ entity?.address?.split(',')[0] }}
-          </h5>
-        </ion-text>
-        <ion-text class="d-flex" style="position: relative; padding-top: 15px">
-          <ion-avatar class="avatar">
-            <img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y">
-          </ion-avatar>
-          <span class="ion-align-self-center">Boris Vicena</span>
-          <span class="ion-align-self-center time-posted">2 weeks ago</span>
-        </ion-text>
-
-        <ion-text>
-          <h1 class="description">
-            Popis
-          </h1>
-        </ion-text>
-        <ion-text
-          color="medium"
-        >
-          <h5>
-            Malý skatepark, často preplnený.
-            Nachádza sa tu: Rail, Down Rail, Manual Box, Pyramída
-            Otvorené non-stop
-          </h5>
-        </ion-text>
-
-        <div id="map-container-report" class="map-container-report" />
-
-        <ion-text class="ion-align-self-center">
-          <p style="text-align: center; font-weight: bold; margin-bottom: 0">
-            Niečo nie je správne ?
+      <ion-text
+        color="medium"
+        class="ion-text-center"
+      >
+        <h5 class="font-weight-bold ion-align-self-center ion-no-margin">
+          {{ entity?.address?.split(',')[0] }}
+        </h5>
+      </ion-text>
+      <div class="d-flex ion-align-items-center">
+        <ion-avatar class="ion-margin-end avatar">
+          <img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y">
+        </ion-avatar>
+        <ion-text class="d-flex ion-justify-content-between flex-grow">
+          <p>{{ entity?.author?.name }}</p>
+          <p>
+            2 weeks ago *
           </p>
-          <div class="upravy-button">
-            <ion-button color="tertiary ion-padding">
-              Navrhnúť úpravu
-            </ion-button>
-          </div>
         </ion-text>
+      </div>
 
-        <!-- <ion-text class="d-flex ion-margin-top">
+      <ion-text>
+        <h1 class="ion-margin-top">
+          Popis
+        </h1>
+      </ion-text>
+      <ion-text
+        color="medium"
+      >
+        <h5>
+          Malý skatepark, často preplnený.
+          Nachádza sa tu: Rail, Down Rail, Manual Box, Pyramída
+          Otvorené non-stop *
+        </h5>
+      </ion-text>
+
+      <ion-text>
+        <h1 class="ion-margin-top">
+          Poloha
+        </h1>
+      </ion-text>
+
+      <div id="map-container-report" class="map-container-report" />
+
+      <!-- <ion-text class="d-flex ion-margin-top">
           <h1
             class="ion-no-margin ion ion-align-self-center"
           >
             {{ entity?.category.full_name }}
           </h1>
         </ion-text> -->
-      </div>
     </ion-content>
   </ion-page>
 </template>
@@ -97,6 +94,7 @@ import {
   IonTitle,
   IonBackButton,
   modalController,
+  IonAvatar,
 } from '@ionic/vue'
 
 import { flagOutline, locationOutline, mapOutline } from 'ionicons/icons'
@@ -118,6 +116,7 @@ export default defineComponent({
     IonToolbar,
     IonTitle,
     IonBackButton,
+    IonAvatar,
   },
   data() {
     return {
@@ -171,43 +170,24 @@ export default defineComponent({
 
 </script>
 
-<style lang="scss">
-
-.image {
-  overflow: hidden;
-  border-radius: 15px !important;
+<style lang="scss" scoped>
+.image-border-radius::part(image) {
+  border-radius: 1rem;
 }
 
-.avatar  {
-width: 50% !important;
-height : 50% !important;
-max-width: 50px !important;
-max-height: 50px !important;
-padding-right: 10px;
-}
-
-.time-posted {
-  position: absolute;
-  right: 0px;
-}
-
-.description {
-  margin-top: 40px;
+.avatar {
+  height: 2.5rem;
+  width: 2.5rem;
 }
 
 .map-container-report {
-  border-radius: 15px;
-  height: 70vw;
-  width: 75%;
-  margin: auto;
-  margin-top: 60px;
-  padding: 10px;
+  aspect-ratio: 1 / 1;
+  border-radius: 1rem;
+  margin-top: 1rem !important;
+  width: 100%;
 }
 
-.upravy-button {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+.flex-grow {
+  flex-grow: 1;
 }
-
 </style>
