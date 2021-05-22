0ddfe9718c7b535f70fcc4cbee3f2dae68a6b839
Jakub Jelínek <jakubkoje@gmail.com>
2021-05-22 14:30:47 +0200

    fixed apple login

--- src/plugins/jakub@capacitor/apple-login/index.ts
+++ src/plugins/jakub@capacitor/apple-login/index.ts
@@ -2,4 +2,5 @@ import { registerWebPlugin } from '@capacitor/core'
 
 import { SignInWithApple } from '@capacitor-community/apple-sign-in'
 
+// @ts-expect-error Outdated plugin
 registerWebPlugin(SignInWithApple)
